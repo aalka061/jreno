53e12d784383c16e442d662e95176b1220451c66
homechiefs <aalkaf@smartbuildingapps.com>
2022-09-19 15:22:15 -0400

    finish maintenance page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'pages/contactpage'
   get 'pages/construction'
   get 'pages/renovation'
+  get 'pages/maintenance'
   root to: 'pages#homepage'
   resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
